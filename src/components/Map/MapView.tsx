2945159aebad008e08a4e18d0b7084c6aa85a51d
lilpissbaby <97313267+lilpissbaby@users.noreply.github.com>
2026-03-30 18:47:24 +0200

    Refactor MapView to use a constant for center

--- src/components/Map/MapView.tsx
+++ src/components/Map/MapView.tsx
@@ -1,14 +1,17 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
+import { LatLngExpression } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
 type Props = {
   tileUrl: string
 }
 
+const center: LatLngExpression = [41.1189, 1.2445]
+
 export default function MapView({ tileUrl }: Props) {
   return (
     <MapContainer
-      center={[41.1189, 1.2445] as [number, number]}
+      center={center}
       zoom={13}
       style={{ height: '100vh', width: '100%' }}
     >
