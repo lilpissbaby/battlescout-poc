ac0d0386473893817ccbf0c4038c48e16044008a
lilpissbaby <klymmaxim@gmail.com>
2026-03-30 19:13:44 +0200

    revert a primera prueba de mapas

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    headers: {
-      'Content-Security-Policy': "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:"
-    }
-  }
-})
