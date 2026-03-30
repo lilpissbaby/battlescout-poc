d60fabc5fc3551a5cb39ad056c06a36a186ce41c
lilpissbaby <97313267+lilpissbaby@users.noreply.github.com>
2026-03-30 19:01:39 +0200

    Refactor MapView component to set center and zoom

--- src/components/Map/MapView.tsx
+++ src/components/Map/MapView.tsx
@@ -1,18 +1,16 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
-import { LatLngExpression } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
 type Props = {
   tileUrl: string
 }
 
-//const center: LatLngExpression = [41.1189, 1.2445]
-
 export default function MapView({ tileUrl }: Props) {
   return (
     <MapContainer
-      //zoom={13}
-      //style={{ height: '100vh', width: '100%' }}
+      center={[41.1189, 1.2445]}
+      zoom={13}
+      style={{ height: '100vh', width: '100%' }}
     >
       <TileLayer url={tileUrl} />
     </MapContainer>
